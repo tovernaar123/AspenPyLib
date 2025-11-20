a4c9655a36218b45878c5c34045bf0bd6862023a
tovernaar123 <ben.schellevis@outlook.com>
2025-11-20 16:07:00 +0100

    removed useless comments

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -5,9 +5,6 @@ import win32com.client as win32
 @pytest.fixture(scope="class")
 def aspen():
     aspen = win32.gencache.EnsureDispatch("Apwn.Document.38.0") # type: ignore
-    # aspen.InitNew2()
-    # aspen.Visible = False
-    # aspen.SuppressDialogs = True 
     yield aspen
     aspen.Quit()
 
@@ -15,12 +12,6 @@ def aspen():
 def AspenOpen(aspen, request):
     dir_path = os.path.dirname(os.path.realpath(__file__))
     file = request.cls.FILE
-
-    # Reset Aspen enough to load a new file,
-    # but NOT so much that COM disconnects
-
-
-    # Now load the case
     aspen.InitFromArchive3(f"{dir_path}\\..\\aspen_files\\{file}")
     aspen.Visible = False
     aspen.SuppressDialogs = True
