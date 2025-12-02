6524579a691aa5ef6efa8f2bb3491cfe66b956ea
florisgus <ftegussinklo@gmail.com>
2025-12-02 12:27:33 +0100

    OPEX this

---
+++ src/Opex.py
@@ -0,0 +1,13 @@
+from aspen import Aspen
+
+def MakeOpex(input)->dict:
+    '''
+    creates an default Opex dict needed for TEA?
+    
+    :param input: Description
+    :return: Description
+    :rtype: dict
+    '''
+    placeholder = {}
+    return placeholder
+    
