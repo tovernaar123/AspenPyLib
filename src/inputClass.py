f6d99e77e0d3949a89e82c0ea61cf6214ca11467
florisgus <ftegussinklo@gmail.com>
2025-12-02 12:08:58 +0100

    made template for the

---
+++ src/inputClass.py
@@ -0,0 +1,29 @@
+from aspen import read_data, Aspen
+
+def getinputClass(data:dict=None,Aspen:Aspen = None) -> dict:
+    '''
+    uses the dict or Aspen to generate an Dict for input in the TEA package
+    If both are input then the data:dict will be prefferd
+
+    
+    :param data: Description
+    :type data: dict
+    :param Aspen: Description
+    :type Aspen: Aspen
+    :return: Description
+    :rtype: dict
+    '''
+    if data == None and Aspen == None:
+        Exception("eather data or Aspen should be an input")
+    elif data != None:
+        print("this should do things")
+    elif Aspen != None:
+        data = read_data(Aspen)
+    
+    placeholder = {}
+    return placeholder
+
+
+def makeCustonClass(input)->dict:
+    placeholder = {}
+    return placeholder
