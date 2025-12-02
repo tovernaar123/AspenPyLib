211bc255eadf748484a26468886c44257566d785
florisgus <ftegussinklo@gmail.com>
2025-12-02 12:09:47 +0100

    Fetch them units for the hole PLACEHOLDER

--- src/unitFetch.py
+++ src/unitFetch.py
@@ -1,5 +1,13 @@
-from aspen import *
+from aspen import Aspen
 
-def unitfetch(aspen:Aspen):
+def unitfetch(aspen:Aspen)-> dict:
+    '''
+    this function should return all units used in the current aspen file
+    
+    :param aspen: Description
+    :type aspen: Aspen
+    :return: Description
+    :rtype: dict
+    '''
     placeholder = {}
     return placeholder
