0582f9172902e7f82bb4b32b92d0e2b6d2da5ea6
florisgus <ftegussinklo@gmail.com>
2025-12-02 12:29:53 +0100

    added a final resting place for all things calling the TEA Package

---
+++ src/TeaIntegration.py
@@ -0,0 +1,5 @@
+from aspen import read_data, Aspen
+import Opex as opx
+import inputClass as ipc
+import unitFetch as uFetch
+'''this will be the final resting place for all things that call the TEA Package'''
