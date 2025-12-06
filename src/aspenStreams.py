7b4962261321249f7150e1e6f13876dae0b68343
florisgus <ftegussinklo@gmail.com>
2025-12-06 15:58:11 +0100

    still only outputs None :(

---
+++ src/aspenStreams.py
@@ -0,0 +1,43 @@
+from aspen import Aspen, read_data
+from pprint import pprint
+
+def GetStreams(aspen: Aspen, data = None):
+    '''
+    Docstring for GetStreams
+    
+    :param aspen: Description
+    :type aspen: Aspen
+    :param data: Description
+    '''
+    if data == None:
+        data = read_data(aspen)
+        # pprint(data)
+    
+    blocks = data.keys()
+    # for i in blocks:
+    #     for j in range()
+    for i in blocks:
+        print(rf"{i}\Connections")
+        streams = aspen.Application.Tree.FindNode(rf"{i}\Connections")
+        print(streams)
+    
+    # return streams
+    
+if __name__ == "__main__":
+    from aspen import init_aspen, run_aspen
+    import sys
+    import os.path as op
+    print("start")
+    if len(sys.argv) < 2:
+        print("Should be called with the name of the aspen file")
+        exit(1)
+    
+    
+    aspen = init_aspen(op.abspath(sys.argv[1]))
+    print("aspin initialized")
+    
+    # run_aspen(aspen=aspen)
+    print("done\ngetting streams")
+    
+    GetStreams(aspen=aspen)
+    
